3993841ffcf5c081ebec27a217f9a2c9813780d0
IORD1 <pratham111ingole@gmail.com>
2022-09-10 08:27:51 +0530

    still solving issues with firebase

--- public/app.js
+++ public/app.js
@@ -1,47 +1,47 @@
-///// User Authentication /////
+// ///// User Authentication /////
 
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 const auth = getAuth();
+alert("authed");
 
-const whenSignedIn = document.getElementById('whenSignedIn');
-const whenSignedOut = document.getElementById('whenSignedOut');
+// const whenSignedIn = document.getElementById('whenSignedIn');
+// const whenSignedOut = document.getElementById('whenSignedOut');
 
-const signInBtn = document.getElementById('signInBtn');
-const signOutBtn = document.getElementById('signOutBtn');
+// const signInBtn = document.getElementById('signInBtn');
+// const signOutBtn = document.getElementById('signOutBtn');
 
-const userDetails = document.getElementById('userDetails');
-
-
-const provider = new firebase.auth.GoogleAuthProvider();
-
-/// Sign in event handlers
+// const userDetails = document.getElementById('userDetails');
 
 
-signInBtn.onclick = () => auth.signInWithPopup(provider);
+// const provider = new firebase.auth.GoogleAuthProvider();
 
-signOutBtn.onclick = () => auth.signOut();
+// /// Sign in event handlers
 
-signInWithPopup(auth, provider)
-  .then((result) => {
 
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
+// signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+// signOutBtn.onclick = () => auth.signOut();
+
+// signInWithPopup(auth, provider)
+//   .then((result) => {
+
+//     // This gives you a Google Access Token. You can use it to access the Google API.
+//     const credential = GoogleAuthProvider.credentialFromResult(result);
+//     const token = credential.accessToken;
+//     // The signed-in user info.
+//     const user = result.user;
+//     // ...
+//   }).catch((error) => {
+//     // Handle Errors here.
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // The email of the user's account used.
+//     const email = error.customData.email;
+//     // The AuthCredential type that was used.
+//     const credential = GoogleAuthProvider.credentialFromError(error);
+//     // ...
+//   });
 
   function func(){
-    alert();
+    alert("func here");
     
   }
