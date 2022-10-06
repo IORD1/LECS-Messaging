b72dfa09f82e26a5c161a50a27baa531e033e454
IORD1 <pratham111ingole@gmail.com>
2022-10-06 22:23:43 +0530

    working

--- public/index.js
+++ public/index.js
@@ -1,19 +1,8 @@
 
-alert("staring import for app.js");
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.6.9/firebase-app.js";
-import {
-  getFirestore,collection,addDoc,doc,setDoc,getDocs
-} from "https://www.gstatic.com/firebasejs/9.6.9/firebase-firestore.js";
-import {
-getAuth,
-GoogleAuthProvider,
-signInWithRedirect,
-getRedirectResult,
-signInWithPopup,
-signOut
-} from "https://www.gstatic.com/firebasejs/9.6.9/firebase-auth.js";
-alert("imported for app,");
+
+
+
 
 function sendtext(){
-    console.log("clicked");
-  }
+  document.getElementById("display").innerHTML = 'sended text';
+}
