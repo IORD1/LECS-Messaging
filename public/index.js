e1ae1aac31e6bfdafd7f48045c07183974997d91
IORD1 <pratham111ingole@gmail.com>
2022-10-07 23:56:19 +0530

    working on database

--- public/index.js
+++ public/index.js
@@ -1,8 +1,16 @@
 
 
 
-
-
 function sendtext(){
   document.getElementById("display").innerHTML = 'sended text';
 }
+
+  try {
+      const docRef = await addDoc(collection(db, "test"), {
+      sender : 'pratham'
+        });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+// Add a new document in collection "cities"
